b9b1618ad41906568097972d02b98d9cde7ee2cc
Gregoire-Mouilleau <gregoire.mouilleau@gmail.com>
2024-03-05 14:36:59 +0100

    Création du JS - Partie 1

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-'use strict';
+// ajouter possibilité de choisir le nb min & max -) influence la nb min max dans le html
+// faire un if else pour modifier le score qu'on perd par mauvais choix
+// retirer virgules etc
+
+const guessInput = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const message = document.querySelector('.message');
+const scoreLabel = document.querySelector('.score');
+const highscoreLabel = document.querySelector('.highscore');
+const numberDisplay = document.querySelector('.number');
+const againButton = document.querySelector('.again');
+
+let secretNumber = Math.floor(Math.random() * 21) + 1;
+let score = 20;
+let highscore = 0;
+
+
+function displayMessage(msg) {
+  message.textContent = msg;
+}
+
+function updateScore(newScore) {
+  score = newScore;
+  scoreLabel.textContent = score;
+}
+
+function displayNumber(number) {
+  numberDisplay.textContent = number;
+}
+
+checkButton.addEventListener('click', function() {
+  const guess = Number(guessInput.value);
+
+  if (guess < 1 || guess > 20 || isNaN(guess)) {
+    displayMessage('⛔️ Aucun numéro!');
+  } else if (guess === secretNumber) { 
+    displayMessage('🎉 Bravo! Vous avez deviné le numéro!');
+    displayNumber(secretNumber);
+    
+    document.body.style.backgroundColor = '#60b347';
+    numberDisplay.style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      highscoreLabel.textContent = highscore;
+    }
+  } else if (guess !== secretNumber) { 
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Trop haut!' : '📉 Trop bas!');
+      updateScore(score - 1);
+    } else {
+      displayMessage('💥 Vous avez perdu le jeu!');
+      updateScore(0);
+    }
+  }
+});
+
+againButton.addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  updateScore(score);
+  displayNumber('?');
+  guessInput.value = '';
+  document.body.style.backgroundColor = '#222';
+  numberDisplay.style.width = '15rem';
+});
